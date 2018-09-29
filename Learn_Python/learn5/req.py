1da84c919b00c904bd3f7659f6d4cf7e030fb250
Khmelnitskiy Mikhail <slychai85@gmail.com>
2018-09-29 15:15:38 +0300

    Получаем данные о температуре в Москве в Цельсиях и выводим на главной странице текущую дату, город и температуру

--- Learn_Python/learn5/req.py
+++ Learn_Python/learn5/req.py
@@ -10,7 +10,7 @@ def get_weather( url ):
 
 # Если функцию вызываем напрямую, то вызываем наш url
 if __name__ == "__main__":
-    data = get_weather( "http://api.openweathermap.org/data/2.5/weather?q=Moscow&APPID=be37def33fbf342b7cb451e62381a38c" )
+    data = get_weather( "http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=be37def33fbf342b7cb451e62381a38c&units=metric")
     print( data )
 
 
--- Learn_Python/learn5/server.py
+++ Learn_Python/learn5/server.py
@@ -1,4 +1,12 @@
+from datetime import datetime
 from flask import Flask
+
+# Импортирую свою функцию
+from req import get_weather
+
+# Переменные для подставления в url
+city_id = 524901
+apikey = "be37def33fbf342b7cb451e62381a38c"
 
 # Переменная которая содержит каркас приложения (создание приложения)
 app = Flask(__name__)
@@ -8,7 +16,13 @@ app = Flask(__name__)
 
 # Обработчик для адреса
 def index():
-    return "Hi!"
+    url = "http://api.openweathermap.org/data/2.5/weather?id=%s&APPID=%s&units=metric" % ( city_id, apikey )
+    weather = get_weather( url )
+    cur_date = datetime.now().strftime('%d.%m.%Y')
+    result = "<p><b>Температура:</b> %s</p>" % ( weather['main']['temp'])
+    result += "<p><b>Город:</b> %s</p>" % ( weather['name'] )
+    result += "<p><b>Дата:</b> %s</p>" % ( cur_date )
+    return result
 
 # Если запускается напрямую, его нужно запустить
 if __name__ == "__main__":
