eb5a137492bff0a4d77543be76ed88128c6da2fc
Khmelnitskiy Mikhail <slychai85@gmail.com>
2018-09-29 14:36:48 +0300

    Исправлен на более корректный запрос

--- Learn_Python/learn5/req.py
+++ Learn_Python/learn5/req.py
@@ -3,10 +3,14 @@ import requests
 # Функция выводит результат запроса url
 def get_weather( url ):
     result = requests.get( url )
-    print( result.text )
+    if result.status_code == 200:
+        return result.json()
+    else:
+        print("Где-то ошибка")
 
 # Если функцию вызываем напрямую, то вызываем наш url
 if __name__ == "__main__":
-    get_weather( "http://api.openweathermap.org/data/2.5/weather?q=Moscow&APPID=be37def33fbf342b7cb451e62381a38c" )
+    data = get_weather( "http://api.openweathermap.org/data/2.5/weather?q=Moscow&APPID=be37def33fbf342b7cb451e62381a38c" )
+    print( data )
 
 
