7bc887e613a4dcfa44607026723fe83a9c970dbd
Khmelnitskiy Mikhail <slychai85@gmail.com>
2018-10-19 00:16:50 +0300

    Error - не получается запустить парсинг документа

--- Learn_Python/Projects/parse.py
+++ Learn_Python/Projects/parse.py
@@ -1,13 +1,17 @@
 from bs4 import BeautifulSoup
 # Работа с датой, нужно чтобы получать дату здесь и сейчас
 from datetime import datetime
-
 # Импорт функции, которая находится в req, для того, чтобы получить html сайта
 from req import get_html
 
 # Переменная хранит дату сегодняшнего дня
 dt_now = datetime.now()
-
+# Начало ссылки 
+short_link = 'www.zakupki.gov.ru'
+# Переменная будет хранить все нужные ссылки
+list_links = [];
+# Переменная для записи id которые выдали ошибку
+error_links = []
 # Составляем переменные url, чтобы составить правильный запрос
 # af=on - подача заявок
 # ca=on - работа комиссии
@@ -28,33 +32,37 @@ params = {
         'updateDateFrom': dt_now.strftime('%d.%m.%Y'),
         'updateDateTo': dt_now.strftime('%d.%m.%Y'),
         'priceFromGeneral': '1000000',
-        'priceToGeneral': ''
+        'priceToGeneral': '2222222222'
 }
-test_params = {
-        'pageNumber': 1,
+for item in range(1, 20):
+    test_params = {
+        'pageNumber': item,
         'recordsPerPage': '_50',
         'fz44': 'on',
         'pc': 'on', 
         'updateDateFrom': dt_now.strftime('%d.%m.%Y'),
         'updateDateTo': dt_now.strftime('%d.%m.%Y'),
-        'priceFromGeneral': 100000000,
-        'priceToGeneral': 1000000000
-}
-url = 'http://www.zakupki.gov.ru/epz/order/extendedsearch/results.html?pageNumber={0}&recordsPerPage={1}&fz44={2}&pc={3}&priceFromGeneral={4}&priceToGeneral={5}&updateDateFrom=17.10.2018&updateDateTo=17.10.2018'.format(test_params['pageNumber'], test_params['recordsPerPage'], test_params['fz44'], test_params['pc'], test_params['priceFromGeneral'], test_params['priceToGeneral'])
-# html страница Госзакупок по закону 44-ФЗ с сегодняшней датой обновления
-# for item in range(1, 20):
-#     params['pageNumber'] = item
-html = get_html(url)
+        'priceFromGeneral': '100000000',
+        'priceToGeneral': '1000000000'
+    }
+    html = get_html('http://www.zakupki.gov.ru/epz/order/extendedsearch/results.html', params=test_params)
+    # Парсим страницу
+    bs = BeautifulSoup( html, 'html.parser')
+    # Проходим циклом и вытягиваем ссылки на тендеры
+    for item in bs.find_all('td', class_='descriptTenderTd'):
+        href = item.find('a')
+        try:
+            list_links.append(href.get('href'))
+        except:
+            error_links.append(href)
 
-# Парсим страницу
-bs = BeautifulSoup( html, 'html.parser')
-
-# Проходим циклом и вытягиваем ссылки на тендеры
-for item in bs.find_all('td', class_='descriptTenderTd'):
-    href = item.find('a')
-    try:
-        print(href.get('href'))
-    except AttributeError as e:
-        print('Ошибка при запросе: {}'.format( e ) )
-
-urlllll = 'http://www.zakupki.gov.ru/epz/order/extendedsearch/results.html?pageNumber={0}&recordsPerPage={1}&fz44={2}&pc={3}&priceFromGeneral={4}&priceToGeneral={5}&updateDateFrom={6}&updateDateTo={7}'.format(test_params['pageNumber'], test_params['recordsPerPage'], test_params['fz44'], test_params['pc'], test_params['priceFromGeneral'], test_params['priceToGeneral'], test_params['updateDateFrom'], test_params['updateDateTo'])
+for link in list_links:
+    common_url = short_link + link
+    print(common_url)
+    htmls = get_html(common_url, params=params)
+    bs = BeautifulSoup(htmls, 'html.parser')
+    for item in bs.find_all('td', class_ = 'noticeTdFirst fontBoldTextTd'):
+        if item == 'Способ определения поставщика (подрядчика, исполнителя)':
+            info = item.find_next_sibling('td')
+            print(info)
+# http://www.zakupki.gov.ru/epz/order/extendedsearch/results.html?pageNumber=1&recordsPerPage=_50&fz44=on&fz223=on&ppRf615=on&af=on&ca=on&pc=on&pa=on&priceFromGeneral=1111111111&priceToGeneral=22222222222&updateDateFrom=18.10.2018&updateDateTo=18.10.2018
